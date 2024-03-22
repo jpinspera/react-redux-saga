2297f91043cd18a070b589c8355b2167a2e1e62a
Jai Parkash <jai.parkash@inspera.no>
2024-03-22 20:19:33 +0530

    chore: remove cart and empty cart

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,9 +1,24 @@
-import { ADD_TO_CART, REMOVE_TO_CART } from "./constraints";
+import { ADD_TO_CART, EMPTY_CART, REMOVE_FROM_CART } from "./constraints";
 
 export const addToCart = (data) =>{
-    console.warn("action called", data);
+    console.warn("add to cart action called", data);
     return {
         type: ADD_TO_CART,
         data
+    }
+}
+
+export const removeFromCart = (data) =>{
+    console.warn("remove from cart action called", data);
+    return {
+        type: REMOVE_FROM_CART,
+        data
+    }
+}
+
+export const emptyCart = () =>{
+    console.warn("empty cart action called");
+    return {
+        type: EMPTY_CART
     }
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,18 +1,20 @@
-import { ADD_TO_CART, REMOVE_TO_CART } from "./constraints";
+import { ADD_TO_CART, EMPTY_CART, REMOVE_FROM_CART } from "./constraints";
 
 export const cartData = (data = [], action) => {
-  // if(action.type === ADD_TO_CART){
-  //     console.warn("Reducer called",action)
-  //     return action.data;
-  // }else{
-  //     return "no action matched"
-  // }
-
   switch (action.type) {
     case ADD_TO_CART:
-      //Add to cart logic
       console.warn("Add to cart condition", action);
       return [action.data, ...data];
+
+    case REMOVE_FROM_CART:
+      console.warn("REMOVE_FROM_CART condition", action);
+      data.length = data.length ? data.length - 1 : [];
+      return [...data];
+
+    case EMPTY_CART:
+      console.warn("EMPTY_CART condition", action);
+      data=[]
+      return [...data];
     default:
       return data;
   }
