e5f2e927cc217e41edcc603e5625cd2d4cfcfdd9
Jai Parkash <jai.parkash@inspera.no>
2024-03-23 15:17:39 +0530

    chore: Configure saga

--- src/redux/productAction.js
+++ src/redux/productAction.js
@@ -1,9 +1,8 @@
 import { PRODUCT_LIST } from "./constraints";
 
-export const productList = () =>{
-    // let data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    // data = data.json();
-    let data = "hello";
+export const productList = async() =>{
+    let data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    data = data.json();
     console.warn("product list action called", data);
     return {
         type: PRODUCT_LIST,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
-// import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import productSaga from "./productSaga";
+import createSagaMiddleware from "redux-saga";
 
-// const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: ()=> [sagaMiddleware]
 })
 
+sagaMiddleware.run(productSaga);
 export default store;
