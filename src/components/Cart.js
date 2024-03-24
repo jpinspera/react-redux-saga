ce15c885ab1c1155c49038b302e36b6050e10c97
Jai Parkash <jai.parkash@inspera.no>
2024-03-24 20:17:26 +0530

    chore: Setup routing and add cart page

---
+++ src/components/Cart.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+  return (
+    <div>
+      <Link to={"/"}>
+        Product List
+      </Link>
+      <h1>Cart Page</h1>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,25 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-const Header = () =>{
-    const result = useSelector((state)=> state.cartData);
-    console.warn("redux data in header", result)
-    return (
-        <div className="header">
-            <div className="cart-div">
-                <span>{result.length}</span>
-                <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="" />
-            </div>
+const Header = () => {
+  const result = useSelector((state) => state.cartData);
+  console.warn("redux data in header", result);
+  return (
+    <div className="header">
+      <Link to={"/"}>
+        <h1 className="logo">E-Comm</h1>
+      </Link>
+      <Link to={"/cart"} relative="path">
+        <div className="cart-div">
+          <span>{result.length}</span>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
+            alt=""
+          />
         </div>
-    )
-}
+      </Link>
+    </div>
+  );
+};
 
 export default Header;
